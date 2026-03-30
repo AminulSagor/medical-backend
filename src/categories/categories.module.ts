03911de5513c69218864c3ab76a8e81ef91c3f63
decodersifat <user.sifathossain@gmail.com>
2026-03-30 11:42:42 +0600

    Merge /admin/categories to use BlogCategory entity

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,13 +1,13 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { Category } from "./entities/category.entity";
+import { BlogCategory } from "../blog-categories/entities/blog-category.entity";
 import { CategoriesController } from "./categories.controller";
 import { CategoriesService } from "./categories.service";
 import { RolesGuard } from "../auth/guards/roles.guard";
 import { User } from "../users/entities/user.entity";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Category, User])],
+    imports: [TypeOrmModule.forFeature([BlogCategory, User])],
     controllers: [CategoriesController],
     providers: [CategoriesService, RolesGuard],
 })
