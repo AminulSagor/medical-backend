6d2e384b33ef31073e5d8752e43aa7a07b6a727f
Habibur Rahman Masum <hrmasum99.study@gmail.com>
2026-05-03 12:21:38 +0600

    fix: delete unnecessary parts in newsletter

--- src/newsletters/audience/dto/add-segment-members.dto.ts
+++
@@ -1,9 +0,0 @@
-// import { ArrayMaxSize, ArrayUnique, IsArray, IsUUID } from 'class-validator';
-
-// export class AddSegmentMembersDto {
-//   @IsArray()
-//   @ArrayUnique()
-//   @ArrayMaxSize(1000)
-//   @IsUUID('4', { each: true })
-//   subscriberIds: string[];
-// }
--- src/newsletters/audience/dto/list-segments-query.dto.ts
+++
@@ -1,9 +0,0 @@
-// import { IsOptional, IsString, MaxLength } from 'class-validator';
-// import { PaginationQueryDto } from '../../../common/dto/pagination-query.dto';
-
-// export class ListSegmentsQueryDto extends PaginationQueryDto {
-//   @IsOptional()
-//   @IsString()
-//   @MaxLength(120)
-//   search?: string;
-// }
